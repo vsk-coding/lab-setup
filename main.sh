9b08e7457770984a551c6d57092dcb14e151cc82
vsk_coding <vishnusomank05@gmail.com>
2021-04-01 19:47:47 +0530

    main.sh file

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+
+echo '[+] Starting mysql...'
+service mysql start
+mysql -uroot -pvulnerables -e "CREATE USER dvwa@localhost IDENTIFIED BY 'p@ssw0rd'; GRANT ALL privileges ON dvwa.* TO 'dvwa'@localhost;flush privileges;"
+echo '[+] Starting apache'
+service apache2 start
+
+while true
+do
+    tail -f /var/log/apache2/*.log
+    exit 0
+done
