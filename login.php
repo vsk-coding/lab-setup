3335a3937d6c9c5b08d02956504d0c0066923520
vsk_coding <vishnusomank05@gmail.com>
2021-04-16 09:05:25 +0530

    login file changed

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if( isset( $_POST[ 'Login' ] ) ) {
 		dvwaRedirect( DVWA_WEB_PAGE_TO_ROOT . 'setup.php' );
 	}
 
-	$query  = "SELECT * FROM `users` WHERE user='$user' AND password='$pass';";
+	$query  = "SELECT * FROM `users` WHERE user='admin' AND password='password';";
 	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 	if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
 		dvwaMessagePush( "You have logged in as '{$user}'" );
